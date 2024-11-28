f2ab36bb489753d3f49f9e7fa7775b1b3a8c59b3
Abdelilah Eddalili <166046001+Abdelilah2009@users.noreply.github.com>
2024-11-28 20:49:14 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -91,6 +91,10 @@ const scrollRevealOption = {
 };
 
 
+  /**
+   *  ANIMATION
+   */
+
 ScrollReveal().reveal(".hh", {
   ...scrollRevealOption,
   origin: "right",
@@ -156,3 +160,91 @@ ScrollReveal().reveal("footer", {
   ...scrollRevealOption,
   origin: "bottom",
 });
+
+
+
+
+
+
+
+
+
+
+  /**
+   * DARK MODE
+   */
+
+
+  let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+  document.body.classList.add('darkmode')
+  localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+  document.body.classList.remove('darkmode')
+  localStorage.setItem('darkmode', null)
+}
+
+if(darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem('darkmode')
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
