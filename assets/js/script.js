55c1f82b4938a48841561dc18c8b6dfc76c5ad7e
Abdelilah Eddalili <166046001+Abdelilah2009@users.noreply.github.com>
2024-11-30 15:09:18 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 
-
 /**
  * SLIDER
  */
@@ -184,3 +183,62 @@ themeSwitch.addEventListener("click", () => {
   darkmode = localStorage.getItem('darkmode')
   darkmode !== "active" ? enableDarkmode() : disableDarkmode()
 })
+
+
+  /**
+   * CURSOR
+   */
+
+
+  
+
+/**
+ * add event on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+
+/**
+ * Custom cursor
+ */
+
+const cursors = document.querySelectorAll("[data-cursor]");
+const hoveredElements = [...document.querySelectorAll("button"), ...document.querySelectorAll("a")];
+
+window.addEventListener("mousemove", function (event) {
+
+  const posX = event.clientX;
+  const posY = event.clientY;
+
+  /** cursor dot position */
+  cursors[0].style.left = `${posX}px`;
+  cursors[0].style.top = `${posY}px`;
+
+  /** cursor outline position */
+  setTimeout(function () {
+    cursors[1].style.left = `${posX}px`;
+    cursors[1].style.top = `${posY}px`;
+  }, 80);
+
+});
+
+/** add hovered class when mouseover on hoverElements */
+addEventOnElements(hoveredElements, "mouseover", function () {
+  for (let i = 0, len = cursors.length; i < len; i++) {
+    cursors[i].classList.add("hovered");
+  }
+});
+
+/** remove hovered class when mouseout on hoverElements */
+addEventOnElements(hoveredElements, "mouseout", function () {
+  for (let i = 0, len = cursors.length; i < len; i++) {
+    cursors[i].classList.remove("hovered");
+  }
+});
+
